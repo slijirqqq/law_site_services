2156cc67ce139c5c6c9d41231da02bc7de5e02f4
slijirqqq <cult6.cult6.cult6@gmail.com>
2021-03-31 20:54:59 +0300

    fix

--- law_site_project/settings.py
+++ law_site_project/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
