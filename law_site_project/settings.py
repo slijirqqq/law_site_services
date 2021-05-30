6a77a28479da42d9f1874f395de10b0ce3aec1c0
slijirqqq <cult6.cult6.cult6@gmail.com>
2021-05-31 01:07:20 +0300

    Set defult secret key and make migrations for service's atribut name

--- law_site_project/settings.py
+++ law_site_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', '5(-5_))c013_bk14&^sfak5fy*)yppc6h19qlg5url*pui6e0n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG_STATUS')
